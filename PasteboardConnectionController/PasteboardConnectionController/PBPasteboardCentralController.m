4bfd5cb24e4faf73f52b38324577076f79a36073
Max Winde <max.winde@gmail.com>
2013-01-25 19:18:11 +0100

    we have a failing connection!

--- PasteboardConnectionController/PasteboardConnectionController/PBPasteboardCentralController.m
+++ PasteboardConnectionController/PasteboardConnectionController/PBPasteboardCentralController.m
@@ -8,9 +8,17 @@
 
 #import "PBPasteboardCentralController.h"
 
+#if TARGET_OS_IPHONE
+#import <CoreBluetooth/CoreBluetooth.h>
+#else
+#import <IOBluetooth/IOBluetooth.h>
+#endif
+
+
 @interface PBPasteboardCentralController ()
 
-@property (strong) CBCentralManager *centralManager;
+@property (strong, readonly) NSMutableSet *connectedPeripherals;
+@property (strong, readonly) CBCentralManager *centralManager;
 
 @end
 
@@ -29,6 +37,8 @@
     if (self) {
         _name = name;
         _pasteboardServiceUUID = [CBUUID UUIDWithString:@"d6f23f70-66ff-11e2-bcfd-0800200c9a66"];
+        
+        _connectedPeripherals = [[NSMutableSet alloc] init];
         
         _centralManager = [[CBCentralManager alloc] initWithDelegate:self queue:nil];
     }
@@ -80,8 +90,31 @@
                   RSSI:(NSNumber *)RSSI;
 {
     NSLog(@"centralManager:didDiscoverPeripheral: %@\n                   advertisementData: %@\n                                RSSI: %@", peripheral.name, advertisementData, RSSI);
+    
+    if (![self.connectedPeripherals containsObject:peripheral]) {
+        [self.connectedPeripherals addObject:peripheral];
+        [central connectPeripheral:peripheral options:nil];
+    }
 }
 
+- (void)centralManager:(CBCentralManager *)central didConnectPeripheral:(CBPeripheral *)peripheral;
+{
+    NSLog(@"didConnectPeripheral: %@", peripheral.name);
+}
+
+- (void)centralManager:(CBCentralManager *)central didFailToConnectPeripheral:(CBPeripheral *)peripheral error:(NSError *)error;
+{
+    [self.connectedPeripherals removeObject:peripheral];
+    
+    NSLog(@"didFailToConnectPeripheral: %@ error: %@", peripheral.name, error);
+}
+
+- (void)centralManager:(CBCentralManager *)central didDisconnectPeripheral:(CBPeripheral *)peripheral error:(NSError *)error;
+{
+    [self.connectedPeripherals removeObject:peripheral];
+    
+    NSLog(@"didDisconnectPeripheral: %@ error: %@", peripheral.name, error);
+}
 
 
 @end
